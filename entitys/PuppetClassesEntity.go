d8a1eacfc4becab284a817d637e53ce519f13902
0x0bsod <zlodey23@gmail.com>
2019-02-25 19:01:26 +0300

    Add puppetclass getter

---
+++ entitys/PuppetClassesEntity.go
@@ -0,0 +1,41 @@
+package entitys
+
+// SWEs container
+type SWEs struct {
+	HostGroup *SWE   `json:"results"`
+	Total     int    `json:"total"`
+	SubTotal  int    `json:"subtotal"`
+	Page      int    `json:"page"`
+	PerPage   int    `json:"per_page"`
+	Search    string `json:"search"`
+}
+
+// SWE structure
+type SWE struct {
+	ID                  int    `json:"id"`
+	Name                string `json:"name"`
+	Title               string `json:"title"`
+	SubnetID            int    `json:"subnet_id"`
+	SubnetName          string `json:"subnet_name"`
+	OperatingSystemID   int    `json:"operatingsystem_id"`
+	OperatingSystemName string `json:"operatingsystem_name"`
+	DomainID            int    `json:"domain_id"`
+	DomainName          string `json:"domain_name"`
+	EnvironmentID       int    `json:"environment_id"`
+	EnvironmentName     string `json:"environment_name"`
+	ComputeProfileId    int    `json:"compute_profile_id"`
+	ComputeProfileName  string `json:"compute_profile_name"`
+	Ancestry            string `json:"ancestry,omitempty"`
+	PuppetProxyId       int    `json:"puppet_proxy_id"`
+	PuppetCaProxyId     int    `json:"puppet_ca_proxy_id"`
+	PTableId            int    `json:"ptable_id"`
+	PTableName          string `json:"ptable_name"`
+	MediumId            int    `json:"medium_id"`
+	MediumName          string `json:"medium_name"`
+	ArchitectureId      int    `json:"architecture_id"`
+	ArchitectureName    int    `json:"architecture_name"`
+	RealmId             int    `json:"realm_id"`
+	RealmName           string `json:"realm_name"`
+	CreatedAt           string `json:"created_at"`
+	UpdatedAt           string `json:"updated_at"`
+}
