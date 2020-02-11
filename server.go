097358f5b9a5d770fbcefacbd6922a09c8d66914
0x0bsod <zlodey23@gmail.com>
2020-02-11 17:41:41 +0300

    New handlers added

--- server.go
+++ server.go
@@ -54,7 +54,10 @@ func Server(ctx *user.GlobalCTX) {
 	router.HandleFunc("/env/svn/log/{host}/{name}", middleware.Chain(environment.GetSvnLog, middleware.Token(ctx))).Methods("GET")
 	//// GIT
 	router.HandleFunc("/env/git/clone/{host}/{swe}", middleware.Chain(environment.GitCloneHttp, middleware.Token(ctx))).Methods("GET")
-	router.HandleFunc("/env/git/log/{host}/{swe}", middleware.Chain(environment.GitLogHttp, middleware.Token(ctx))).Methods("GET")
+	router.HandleFunc("/env/git/pull/{host}/{swe}", middleware.Chain(environment.GitPullHttp, middleware.Token(ctx))).Methods("GET")
+	router.HandleFunc("/env/git/log/{host}/{swe}/{commit}", middleware.Chain(environment.GitLogCommitHttp, middleware.Token(ctx))).Methods("GET")
+	//router.HandleFunc("/env/git/log/{host}/{swe}", middleware.Chain(environment.GitLogHttp, middleware.Token(ctx))).Methods("GET")
+	router.HandleFunc("/env/git/log/{host}", middleware.Chain(environment.GitLogAllHttp, middleware.Token(ctx))).Methods("GET")
 	//// Foreman
 	router.HandleFunc("/env/{host}", middleware.Chain(environment.GetByHost, middleware.Token(ctx))).Methods("GET")
 	router.HandleFunc("/env", middleware.Chain(environment.GetAll, middleware.Token(ctx))).Methods("GET")
